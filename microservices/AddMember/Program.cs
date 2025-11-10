daf7ca1aae2534ac660350140dc5025ffe0091cc
raulcv <iraulcv@gmail.com>
2025-11-10 03:02:12 -0500

    fix: env variable local nullable v2

--- microservices/AddMember/Program.cs
+++ microservices/AddMember/Program.cs
@@ -17,8 +17,11 @@ app.UseHttpsRedirection();
 
 app.MapPost("/addmember", (string name, string LastName, string birthyear) =>
 {
-    var connectionString = builder.Configuration["ServiceBus:ConnectionString"] ?? Environment.GetEnvironmentVariable("SERVICEBUS_CONNECTION_STRING")!;
-    var queueName = builder.Configuration["ServiceBus:QueueName"] ?? Environment.GetEnvironmentVariable("SERVICEBUS_QUEUE_NAME")!;
+    string connectionString = builder.Configuration["ServiceBus:ConnectionString"] ?? Environment.GetEnvironmentVariable("SERVICEBUS_CONNECTION_STRING")!;
+    string queueName = builder.Configuration["ServiceBus:QueueName"] ?? Environment.GetEnvironmentVariable("SERVICEBUS_QUEUE_NAME")!;
+
+    Console.WriteLine("ASB ConnectionString is null or empty: {0}, QueueName is null or empty: {1}", string.IsNullOrEmpty(connectionString), string.IsNullOrEmpty(queueName)); // var serviceBus = new ServiceBus(connectionString, queueName);
+    
     var serviceBus = new ServiceBus(connectionString, queueName);
     serviceBus.SendMessageAsync(name, LastName, birthyear).GetAwaiter().GetResult();
     return Results.Ok($"Miembro {name} agregado con éxito.");
