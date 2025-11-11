e7c6cf2fe06b9df0e0254cad011565a4e159bbff
raulcv <iraulcv@gmail.com>
2025-11-11 14:30:21 -0500

    fix: appsettings.json dev only added menssages v3

--- microservices/PickAge/Program.cs
+++ microservices/PickAge/Program.cs
@@ -3,22 +3,21 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using PickAge;
 
-IServiceCollection serviceDescriptors = new ServiceCollection();
 
 Host.CreateDefaultBuilder(args)
-   .ConfigureHostConfiguration(configHost =>
-   {
-       // Only load appsettings.json in development
-       if (!args.Contains("--production"))
-       {
-           configHost.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true).Build();
-           Console.WriteLine("Development mode enabled :)");
-       }
-       else
-       {
-           Console.WriteLine("Production mode enabled ;)");
-       }
-   })
+//    .ConfigureHostConfiguration(configHost =>
+//    {
+//        // Only load appsettings.json in development
+//        if (!args.Contains("--production"))
+//        {
+//            configHost.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true).Build();
+//            Console.WriteLine("Development mode enabled :)");
+//        }
+//        else
+//        {
+//            Console.WriteLine("Production mode enabled ;)");
+//        }
+//    })
    .ConfigureServices((hostContext, services) =>
    {
        IConfiguration configuration = hostContext.Configuration;
