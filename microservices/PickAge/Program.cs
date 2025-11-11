a9b37d6ec299fd0bd2a838dce44af7e6dd0d7fc8
raulcv <iraulcv@gmail.com>
2025-11-11 04:43:45 -0500

    fix: appsettings.json dev only

--- microservices/PickAge/Program.cs
+++ microservices/PickAge/Program.cs
@@ -3,12 +3,17 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using PickAge;
 
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
 IServiceCollection serviceDescriptors = new ServiceCollection();
 
 Host.CreateDefaultBuilder(args)
    .ConfigureHostConfiguration(configHost =>
    {
-       configHost.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true).Build();
+    if (app.Environment.IsDevelopment()){
+           configHost.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true).Build();
+       }
    })
    .ConfigureServices((hostContext, services) =>
    {
