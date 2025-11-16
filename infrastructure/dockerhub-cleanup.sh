d813524eebe140a90dfed1ff0bd0c6b9426c5bdc
raulcv <iraulcv@gmail.com>
2025-11-16 00:48:41 -0500

    fix: add docker images clean up script v8

--- infrastructure/dockerhub-cleanup.sh
+++ infrastructure/dockerhub-cleanup.sh
@@ -13,6 +13,11 @@ if [ -z "$DOCKERHUB_USERNAME" ] || [ -z "$DOCKERHUB_PASSWORD" ] || [ -z "$REPOSI
   echo "DOCKERHUB_PASSWORD: ${DOCKERHUB_PASSWORD}"
   echo "REPOSITORY: ${REPOSITORY}"
   exit 1
+else 
+  echo "Environment variables are set."
+  echo "DOCKERHUB_USERNAME: ${DOCKERHUB_USERNAME}"
+  echo "DOCKERHUB_PASSWORD: ${DOCKERHUB_PASSWORD}"
+  echo "REPOSITORY: ${REPOSITORY}"
 fi
 
 KEEP_LAST=${KEEP_LAST:-5}
@@ -27,7 +32,15 @@ KEEP_LAST=${KEEP_LAST:-5}
 
 AUTH_RESPONSE=$(curl -s -H "Content-Type: application/json" -X POST -d "{\"username\": \"$DOCKERHUB_USERNAME\", \"password\": \"$DOCKERHUB_PASSWORD\"}" https://hub.docker.com/v2/users/login/)
 
+if [ -z "$AUTH_RESPONSE" ]; then
+  echo "Failed to get authentication token."
+  echo "Response: $AUTH_RESPONSE"
+  exit 1
+fi
+
 TOKEN=$(echo "$AUTH_RESPONSE" | jq -r .token)
+
+echo "Authentication token: $TOKEN"
 
 if [ "$TOKEN" == "null" ] || [ -z "$TOKEN" ]; then
   echo "Failed to authenticate to Docker Hub. Response:"
@@ -36,7 +49,15 @@ if [ "$TOKEN" == "null" ] || [ -z "$TOKEN" ]; then
 fi
 
 echo "Fetching tags for repository $REPOSITORY..."
-TAGS=$(curl -s -H "Authorization: JWT $TOKEN" "https://hub.docker.com/v2/repositories/$REPOSITORY/tags/?page_size=100" | jq -r '.results | sort_by(.last_updated) | .[].name')
+IMAGES_RESPONSE=$(curl -s -H "Authorization: JWT $TOKEN" "https://hub.docker.com/v2/repositories/$REPOSITORY/tags/?page_size=100")
+
+if [ -z "$IMAGES_RESPONSE" ]; then
+  echo "Failed to fetch tags for repository $REPOSITORY."
+  exit 1
+fi
+
+TAGS=$(echo "$IMAGES_RESPONSE" | jq -r '.results[] | .name')
+
 echo "Fetched tags: $TAGS"
 # Convert tags to array
 TAG_ARRAY=($TAGS)
